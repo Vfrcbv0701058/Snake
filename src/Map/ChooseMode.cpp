4f8ac5fd1453a26c0d44698d3133de3abda5c5ba
mercuric_novel0 <maksym0701058@gmail.com>
2023-09-17 14:59:50 +0200

    fix bag

--- src/Map/ChooseMode.cpp
+++ src/Map/ChooseMode.cpp
@@ -1,8 +1,8 @@
 #include "../../include/maps/ChooseMode.hpp"
 
 bool Choose_mode(){
-    cout << "Enter the level of difficulty (1 - Hard, 2 - Easy)" << endl;
     do {
+        cout << "Enter the level of difficulty (1 - Hard, 2 - Easy)" << endl;
         cin >> diff;
     } while (diff != 1 && diff != 2);
     return (diff == 1); // if diff = 1 it's "true"
